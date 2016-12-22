aa24419434cbba866f6d0f85680cf4fe8cdac600
root <root@localhost.localdomain>
2016-12-22 04:53:18 +0000

    build success

---
+++ install_req.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+yum install perl-ExtUtils-Embed.noarch -y
+perl-ExtUtils-MakeMaker.noarch -y
+yum install python-devel -y
+yum install tcl-devel -y
+yum install openssl-devel -y
+yum install e2fsprogs-devel -y
+yum install libxml2-devel -y
+yum install libxslt-devel -y
+yum install pam-devel -y
+yum install krb5-devel -y
+yum install uuid-devel -y
+yum install openldap-devel -y
+yum install glibc-devel -y
